66645f12d0b7c56bc0e3df525760b163372a4918
d <github.matador258@passmail.net>
2025-12-05 17:17:10 -0500

    add identity file to ssh

--- setup_client.sh
+++ setup_client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Use: send-setup.sh [optional: container name] [optional: remote:container_id]
 lxcname=${1:-"ocelot"}
-container=${1:-"ubuntu:n"}
+container=${2:-"ubuntu:n"}
 lxc launch $container $lxcname
 wait $!
 LOCATIONNAME="/var/snap/lxd/common/lxd/containers/${lxcname}/rootfs/home/ubuntu"
@@ -9,9 +9,10 @@ KEYFILELOCATION="/home/d/.ssh/container-${lxcname}"
 rm -rf $LOCATIONNAME/container-setup-script
 git clone --depth 1 https://github.com/davidthegardens/container-setup-script.git $LOCATIONNAME/container-setup-script
 sudo touch $LOCATIONNAME/.bashrc
-echo 'sudo /home/ubuntu/container-setup-script/setup.sh; exit' | cat - $LOCATIONNAME/.bashrc >temp && mv temp $LOCATIONNAME/.bashrc
+echo 'sudo /home/ubuntu/container-setup-script/setup_container.sh; exit' | cat - $LOCATIONNAME/.bashrc >temp && mv temp $LOCATIONNAME/.bashrc
 
 sudo -u d ssh-keygen -t ed25519 -f $KEYFILELOCATION
+touch "${LOCATIONNAME}/.ssh/authorized_keys"
 cat "${KEYFILELOCATION}.pub" >>"${LOCATIONNAME}/.ssh/authorized_keys"
 ip_addr_unfmt=$(lxc exec $lxcname -- ip addr show eth0 | grep "inet\b" | awk '{print $2}')
 ip_addr="${ip_addr_unfmt:0:-3}"
@@ -20,6 +21,7 @@ lxc exec $lxcname -- sudo -u ubuntu bash
 cat <<EOF >>/home/d/.ssh/config
 
 Host ${lxcname}
+  IdentityFile ${KEYFILELOCATION}.pub
   Hostname ${ip_addr}
   User ubuntu
   LocalForward 3000 localhost:3000
